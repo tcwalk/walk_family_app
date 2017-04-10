3bff01598013eb53dbc53088eb928df6523e2660
Tom Walk <tcwalk@gmail.com>
2017-04-10 17:21:39 -0500

    Fix production root

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    if Rails.env.production?
-      root 'users#index'
-#      root 'application#hello'
+#      root 'users#index'
+      root 'application#hello'
    end
 
 end
