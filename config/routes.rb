77ca995b2efd19c16d59bcfdaa241e9c0fe2f520
Tom Walk <tcwalk@gmail.com>
2017-04-10 16:54:08 -0500

    Initial commit sample_app Ruby-2.4.0 Rail-5.0.2

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   if Rails.env.production?
+      root 'users#index'
+#      root 'application#hello'
+   end
+
+end
