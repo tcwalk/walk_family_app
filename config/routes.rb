391c016e1ed0a54c973a4db883cba462e671f532
Tom Walk <tcwalk@gmail.com>
2017-04-12 19:44:19 -0500

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
 
   get 'static_pages/home'
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-   if Rails.env.production?
-#      root 'users#index'
-      root 'application#hello'
-   end
+#   if Rails.env.production?
+##      root 'users#index'
+#      root 'application#hello'
+      root 'static_pages#home'
+#   end
 
 end
