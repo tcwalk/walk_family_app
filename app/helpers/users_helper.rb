1be54f9ebcdbec11920f475c34d0059c3aa219fc
Tom Walk <tcwalk@gmail.com>
2017-05-23 16:13:27 -0500

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, size: 120)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+#    gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=wavatar&s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+#    debugger
+  end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+
+    assert_template 'users/new'
+#    assert_response :success
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+
+    assert_select 'form[action="/signup"]'
+
+    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
+    assert_select 'div' '#' "error_explanation"
+    assert_select 'div.alert-danger'
+    assert_select "div", {:text=>"The form contains 4 errors."}
+    assert_select "ul li", {:text=>"Name can't be blank"}
+    assert_select "ul li", {:text=>"Email is invalid"}
+    assert_select "ul li", {:text=>"Password confirmation doesn't match Password"}
+    assert_select "ul li", {:text=>"Password is too short (minimum is 6 characters)"}
+#    assert_select "div.field_with_errors", {:count=>8}
+
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.nil?
+
+
+  end
+
+end
+
+#error_explanation
+#.field_with_errors
